0b71738204b97b8643cc2583469026d6c5d4f5fc
John12x2 <muhammadalfianarya@gmail.com>
2025-07-30 20:37:29 +0700

    Added DevSim for devs

---
+++ DevSim/app.js
@@ -0,0 +1,24 @@
+const navToggle = document.querySelector(".nav-toggle");
+const links = document.querySelector(".links");
+
+navToggle.addEventListener("click", function () {
+  // console.log(links.classList.contains("random"));
+  // console.log(links.classList.contains("links"));
+  if (links.classList.contains("show-links")) {
+    links.classList.remove("show-links");
+  } else {
+    links.classList.add("show-links");
+  } 
+});
+
+function copyEmail() {
+  const email = "mail@oneclicks.my.id";
+  navigator.clipboard.writeText(email).then(() => {
+    const msg = document.getElementById("copy-confirm");
+    msg.classList.add("show");
+
+    setTimeout(() => {
+      msg.classList.remove("show");
+    }, 1500);
+  });
+}
